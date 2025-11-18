7454320a38316b10193d53e2d32b60350597dfa2
camilly xavier de souza <xaviercamilly355@gmail.com>
2025-11-18 19:33:05 -0300

    fix: get e set

--- src/main/java/com/senai/gerenciamento_epis/Repo/AdministradoRepository.java
+++ src/main/java/com/senai/gerenciamento_epis/Repo/AdministradoRepository.java
@@ -1,4 +1,10 @@
 package com.senai.gerenciamento_epis.Repo;
 
-public class AdministradoRepository {
+import com.senai.gerenciamento_epis.Entity.AdministradorEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdministradoRepository extends JpaRepository<AdministradorEntity, Integer> {
+
+
+
 }
