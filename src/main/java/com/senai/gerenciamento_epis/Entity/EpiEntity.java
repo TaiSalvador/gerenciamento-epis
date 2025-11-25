68926f815cf9c241890ae0f10590d44e207d049c
Tainá Salvador <tainasalvador71@gmail.com>
2025-11-24 21:26:02 -0300

    fix: CRUD Epi

--- src/main/java/com/senai/gerenciamento_epis/Entity/EpiEntity.java
+++ src/main/java/com/senai/gerenciamento_epis/Entity/EpiEntity.java
@@ -1,10 +1,27 @@
 package com.senai.gerenciamento_epis.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-public class EpiEntity {
+@Table (name = "epis")
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class EpiEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_epis")
+    private int id_epis;
+
+    @Column (name = "nm_epis")
+    private String nm_epis;
 
 }
--- src/main/java/com/senai/gerenciamento_epis/Service/EpisService.java
+++ src/main/java/com/senai/gerenciamento_epis/Service/EpisService.java
@@ -1,4 +1,66 @@
 package com.senai.gerenciamento_epis.Service;
 
+
+import com.senai.gerenciamento_epis.DTO.EpiDTO;
+import com.senai.gerenciamento_epis.Entity.EpiEntity;
+import com.senai.gerenciamento_epis.Repo.ColaboradorRepository;
+import com.senai.gerenciamento_epis.Repo.EpiRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+@Validated
 public class EpisService {
+
+    @Autowired
+    private EpiRepository epiRepository;
+
+    @Autowired
+    private ColaboradorRepository colaboradorRepository;
+
+    //CRUD
+
+    //Create
+
+    public void cadastrarEpi (@Valid EpiDTO  epiDTO) {
+
+        EpiEntity epiEntity = new EpiEntity();
+
+        epiEntity.setNm_epis(epiDTO.getNome());
+
+        epiRepository.save(epiEntity);
+    }
+
+    // Read
+    public List<EpiDTO> listarEpis () {
+
+        List<EpiEntity> listaEpiEntity = epiRepository.findAll();
+
+        List<EpiDTO> listaEpiDTO = new ArrayList<>();
+
+        for (EpiEntity e : listaEpiEntity) {
+
+            EpiDTO epiDTO = new EpiDTO();
+
+            epiDTO.setNome(e.getNm_epis());
+
+            listaEpiDTO.add(epiDTO);
+        }
+
+        return listaEpiDTO;
+    }
+
+    //Update
+
+    //public void atualizaEpi (int id, @Valid EpiDTO epiDTO) {
+        //EpiEntity epiEntity = epiRepository.findById(id).orElseThrow(() -> new RuntimeException("Epi não existe"));
+
+        //if (epiRepository.ex)
+   // }
 }
