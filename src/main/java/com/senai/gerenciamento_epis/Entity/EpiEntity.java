c2b9359f2a26a806ad3e9d339b61eb5b4e091eff
camilly xavier de souza <xaviercamilly355@gmail.com>
2025-11-24 21:28:57 -0300

    fix: correção de erro epientity

--- src/main/java/com/senai/gerenciamento_epis/Entity/EpiEntity.java
+++ src/main/java/com/senai/gerenciamento_epis/Entity/EpiEntity.java
@@ -6,8 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
-<<<<<<< HEAD
 @Table (name = "epis")
 
 @Getter
@@ -24,24 +22,5 @@ public class EpiEntity {
 
     @Column (name = "nm_epis")
     private String nm_epis;
-=======
-@Table(name = "Epi")
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-
-public class EpiEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_Epi")
-    private int idEpi;
-
-    @Column(name = "nm_Emprestimo")
-    private String nmEpi;
-
->>>>>>> f721a461b35a1edcfb4e4776553fff6ceb7a38ed
 
 }
